32cff998a4a4323f4cbcf07429402b3cebe82dd1
shraf-eldeen <eldeenshraf@gmail.com>
2019-11-06 08:46:21 +0200

    initial commit

--- src/main/java/com/samsara/samsara/controllers/AdsApi.java
+++ src/main/java/com/samsara/samsara/controllers/AdsApi.java
@@ -12,7 +12,6 @@ import com.samsara.samsara.services.AdvertiseService;
 import com.samsara.samsara.services.UserService;
 import java.io.File;
 import java.io.IOException;
-import java.net.http.HttpRequest;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
