ff36c1eb0339dfb0b9507a695632ff940874cefa
shraf-eldeen <eldeenshraf@gmail.com>
2019-12-31 02:04:43 +0200

    adding filter

--- src/main/java/com/samsara/samsara/SamsaraApplication.java
+++ src/main/java/com/samsara/samsara/SamsaraApplication.java
@@ -1,11 +1,17 @@
 package com.samsara.samsara;
 
+import com.samsara.samsara.constants.Type;
+import com.samsara.samsara.constants.City;
+import java.net.Proxy;
+import javax.servlet.ServletContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -40,14 +46,20 @@ public class SamsaraApplication {
         return new BCryptPasswordEncoder();
     }
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(SamsaraApplication.class, args);
         LOGGER.info("Simple log statement with inputs {}, {} and {}", 1, 2, 3);
-
     }
+    @EventListener(ApplicationReadyEvent.class)
+public void doSomethingAfterStartup() {
+    System.out.println("hello world, I have just started up");
+    servletContext.setAttribute("msa msa","kywsa");
+    servletContext.setAttribute("types",Type.values());
+    servletContext.setAttribute("cities",City.values());
+}
     @Autowired
     AuthenticationSuccessHandlerImpl onsuccess;
 
+    @Autowired
+    ServletContext servletContext;
 }
